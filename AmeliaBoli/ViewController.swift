116ff31402babf7724cc614e115d592716bd12ab
Amelia Boli <ameliaaboli@gmail.com>
2016-04-19 15:37:48 -0700

    feat: Hook up share button and add images for toolbar

--- AmeliaBoli/ViewController.swift
+++ AmeliaBoli/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController { //, UITapGestureRecognizerDelegate {
         UIApplication.sharedApplication().statusBarStyle = .LightContent
     }
     
+    @IBAction func presentShareController(sender: UIBarButtonItem) {
+        let image = UIImage()
+        let activityView = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.presentViewController(activityView, animated: true, completion: nil)
+    }
+    
     @IBAction func manageToolbar(recognizer: UITapGestureRecognizer) {
         if toolbar.hidden == true {
             toolbar.hidden = false
